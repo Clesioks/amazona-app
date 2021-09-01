f7e368bb18a4581fcd33f1c1314defc2d28041d7
TiTi.Silva <kornalewski@outlook.com>
2021-09-01 18:15:49 -0300

    main

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs'
 const data = {
+    users: [
+        {
+            name: 'Titi',
+            email: 'kornalewski@outlook.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: true
+        },
+        {
+            name: 'Rafa',
+            email: 'rafa@teste.com',
+            password: bcrypt.hashSync('1234', 8),
+            isAdmin: false
+        }  
+    ],
     products: [
         {
             _id: '1',
